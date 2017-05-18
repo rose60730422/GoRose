639b6b8b54f663bf64476b54ed0c6573cb708ff0
Rose <rose.you1214@gmail.com>
2017-05-18 21:34:02 +0800

    taxi practice

---
+++ taxi.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+import "math"
+import "strings"
+import "strconv"
+
+func split(testData string) []float64 {
+	var resultSlice []float64
+	var testDataSlice = strings.Fields(testData)
+	for _, distance := range(testDataSlice) {
+		var x float64
+		if num, err := strconv.ParseFloat(distance, 32); err == nil {
+			x = num
+		}
+		resultSlice = append(resultSlice, x)
+	}
+	return resultSlice
+}
+
+func taxiCount(distance float64) string {
+	var dollars int
+	if distance < 1.5 {
+		dollars = 75
+	} else {
+		var y = int(math.Ceil((distance - 1.5) * 4))
+		dollars = 75 +  y * 5
+	}
+	return fmt.Sprintf("%d\n" , dollars)
+}
+
+func danielAns(testData string) string {
+	myAns := ""
+	problemSet := split(testData) // 把題目拆成一個 slice, 每個元素都是一個題目(int)
+	for _, distance := range(problemSet) {
+		ans := taxiCount(distance)
+		myAns += ans
+	}
+	return myAns
+}
+
+func main() {
+	testData := "0.9\n1.6\n2.2\n3.5\n4.8\n"
+	correctAns := "75\n80\n90\n115\n145\n"
+
+	myAns := danielAns(testData)
+	fmt.Printf("myAns:\n%s", myAns)
+	if myAns == correctAns {
+		fmt.Println("PASS")
+	} else {
+		fmt.Println("FAIL")
+	}
+}
